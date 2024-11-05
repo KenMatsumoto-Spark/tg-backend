25103a49eb9dd65f11d21070849fa8e670dd6c5a
Ken Matsumoto <h264matsumoto@gmail.com>
2024-11-05 20:32:29 -0300

    list care working 2

--- source/features/listCaresByUser.ts
+++ source/features/listCaresByUser.ts
@@ -4,10 +4,11 @@ import 'dotenv/config'
 import Care from '../models/Care'
 import mongoose from 'mongoose'
 import User from '../models/User'
+import Plant from '../models/Plant'
 
 const listCaresByUser = async (userId: string) => {
 
-  const [errorPlants, plants] = await to(User.find({ _id: userId }))
+  const [errorPlants, plants] = await to(Plant.find({ id_usuario: userId }))
   if(errorPlants) throw new Error("erro ao encontrar plantas")
   if(!plants) throw new Error("nenhuma planta encontrada")
 
