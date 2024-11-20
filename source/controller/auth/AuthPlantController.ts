b1b1ab1ef373087e9a9b36340520c1afe8cfe6ef
Dayane Alves <dayaneAlls>
2024-11-19 21:44:15 -0300

    a

--- source/controller/auth/AuthPlantController.ts
+++ source/controller/auth/AuthPlantController.ts
@@ -270,7 +270,7 @@ PlantController.get('/:plantId/activity-care', async (request: Request, response
   }
 })
 PlantController.get('/:plantId/care/list', async (request: Request, response: Response) => {
-  const { plantId } = request.params.plantId
+  const { plantId } = request.params
 
   const invalid = PlantRules.general(
     { plantId }
